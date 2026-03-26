5c254e93c3dc93364e3fa5da493e029bed121604
WillyChang0428 <ancc405000513@gmail.com>
2026-03-26 14:39:08 +0800

    基本資料整理

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <Header />
+  <Footer />
+  
 </template>
