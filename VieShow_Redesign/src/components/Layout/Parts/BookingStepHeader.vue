4f419c458c256f0d4a190d5f79c9f8d310d54e66
WillyChang0428 <ancc405000513@gmail.com>
2026-03-31 16:41:37 +0800

    0331 合併1

--- VieShow_Redesign/src/components/Layout/Parts/BookingStepHeader.vue
+++ VieShow_Redesign/src/components/Layout/Parts/BookingStepHeader.vue
@@ -3,9 +3,23 @@
 </script>
 
 <template>
-  <div class="container">
-    
-  </div>
+  <div class="container-md">
+    <ul class="d-flex justify-content-between align-items-center pt-8">
+      <li class="btn">
+        <!-- <router-link to="/"> -->
+        <i class="fa-solid fa-angle-left text-secondary"></i>
+        <!-- </router-link> -->
+      </li>
+      <li class="text-light">
+        分頁標題
+      </li>
+      <li class="btn">
+        <!-- <router-link to="/home"> -->
+        <i class="fa-solid fa-xmark text-secondary"></i>
+        <!-- </router-link> -->
+    </li>
+    </ul>
+</div>
 </template>
 
 <style lang="scss" scoped>
