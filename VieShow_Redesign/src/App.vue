da8ea4e04eff4b7ed3a56c876044b027f0d9f605
WillyChang0428 <ancc405000513@gmail.com>
2026-03-31 16:38:32 +0800

    家維 0331 下課

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -3,12 +3,27 @@
 </script>
 
 <template>
-  <div id="app">
-    <router-view></router-view>
-    <div class="btn btn-tertiary">4654564</div>
-    </div>
+    <!-- <router-view/> -->
+<div class="container-md">
+    <ul class="d-flex justify-content-between align-items-center pt-8">
+      <li class="btn">
+        <!-- <router-link to="/"> -->
+        <i class="fa-solid fa-angle-left text-secondary"></i>
+        <!-- </router-link> -->
+      </li>
+      <li class="text-light">
+        分頁標題
+      </li>
+      <li class="btn">
+        <!-- <router-link to="/home"> -->
+        <i class="fa-solid fa-xmark text-secondary"></i>
+        <!-- </router-link> -->
+    </li>
+    </ul>
+</div>
 </template>
 
-<style>
+<style lang="scss">
 /* 你的全域樣式 */
+
 </style>
