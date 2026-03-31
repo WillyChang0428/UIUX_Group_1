a3cf03f5747474faf8c834e439d5dddf7fd5a6b9
andylinsally <andylinsally@gmail.com>
2026-03-31 14:29:17 +0800

    變數設定sally

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
+import Header from '@/components/Common/Header.vue'
 </script>
 
 <template>
-<div class="btn btn-primary">123</div>
-<router-view />
-  
+    <div class="main-wrapper">
+        <Header />
+        <main class="app-main">
+            <router-view />
+        </main>
+    </div>
 </template>
 
 <style scoped lang="scss">
-
+.app-main {
+    min-height: 100vh;
+    background: radial-gradient(circle at top, rgba(47, 150, 238, 0.16), #0d1220 80%);
+}
 </style>
--- VieShow_Redesign/src/main.js
+++ VieShow_Redesign/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
 import './assets/scss/main.scss'
 import 'bootstrap'
 import App from './App.vue'
-// import router from './router'
-// import store from './store'
-// createApp(App).use(store).use(router).mount('#app')
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(createPinia())
+    .use(router)
+    .mount('#app')
