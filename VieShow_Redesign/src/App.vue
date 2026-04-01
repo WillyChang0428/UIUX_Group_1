86369e3a7fb6ea0b4dde06c6d94b97f7e1a6724e
andylinsally <andylinsally@gmail.com>
2026-04-01 15:26:11 +0800

    篩選器組件完成

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -1,46 +1,11 @@
 <script setup>
-import { ref } from 'vue'
-import FlatSelect from '@/components/Common/Selecter/FlatSelect.vue'
-import SelectCategory from '@/components/Common/Selecter/SelectCategory.vue'
 
-const isOpen = ref(false)         // 控制選單開關
-const isSelected = ref(false)     // 控制藍色選中樣式
-const selectedValue = ref('')     // 儲存選中的文字內容
-
-// 處理選中動作
-const handleSelect = (val) => {
-  selectedValue.value = val;      // 更新顯示文字
-  isSelected.value = true;        // 切換為藍色高亮
-  isOpen.value = false;           // 自動收合選單
-}
 </script>
 
 <template>
-  <FlatSelect :isOpen="isOpen" :isSelected="isSelected" :placeholder="selectedValue || '預設提示文字'"
-    @toggle="isOpen = !isOpen" @clear="selectedValue = ''; isSelected = false">
-    <template #options>
-      <li @click="handleSelect('選項 A')">選項 A</li>
-      <li @click="handleSelect('選項 B')">選項 B</li>
-      <SelectCategory label="雙北">
-        <li @click="handleSelect('選項 A')">選項 A</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-      </SelectCategory>
-      <SelectCategory label="桃竹苗">
-        <li @click="handleSelect('選項 A')">選項 A</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-      </SelectCategory>
-    </template>
-  </FlatSelect>
+
 </template>
 
-
-
-<style lang="scss" scoped>
+<style lang="scss">
 
 </style>
