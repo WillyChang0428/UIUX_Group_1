aab813efd0e2e4b644b4f2907451f0e1b77ac212
WillyChang0428 <ancc405000513@gmail.com>
2026-04-01 15:58:19 +0800

    重置app.vue

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<template>
-<SignUpStart/>
-<InputBox/>
-</template>
-
-<script setup>
-
-<style lang="scss" scoped>
-=======
 <script setup>
 
 </script>
@@ -18,17 +7,5 @@
 </template>
 
 <style lang="scss">
->>>>>>> 86369e3a7fb6ea0b4dde06c6d94b97f7e1a6724e
-=======
-<script setup>
-// import Header from "./components/Layout/Parts/Header.vue";
-import BookingSelector from "./components/Booking/FastBooking/BookingSelector.vue";
-</script>
 
-<template>
-  <!-- <Header /> -->
-  <BookingSelector />
-</template>
->>>>>>> 4c318a46896fa58e5c14de54129f92c3d1e7da28
-
-<style lang="scss" scoped></style>
+</style>
