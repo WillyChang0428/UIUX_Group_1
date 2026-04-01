cf5d9662dd8a3e667e41b294f72d6f6432889e61
WillyChang0428 <ancc405000513@gmail.com>
2026-04-01 14:49:21 +0800

    大改_variable.scss的內容，以呼應figma變數

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -1,10 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+// 1. 引入剛剛做好的 InputBox
+import MetadataInput from './components/Common/InputBox/MetadataInput.vue';
+</script>
+
 <template>
-  
+    <div class="mb-4">
+      <label class="text-white mb-2 d-block">請輸入您的電子信箱</label>
+      
+      <MetadataInput 
+        v-model="userEmail" 
+        placeholder="例如：abc@gmail.com"
+        type="password"
+      />
+      
+      <p class="text-white-50 mt-2">
+        目前的輸入內容：{{ userEmail }}
+      </p>
+    </div>
 </template>
 
-<script setup>
 
-</script>
 
 <style lang="scss" scoped>
 
