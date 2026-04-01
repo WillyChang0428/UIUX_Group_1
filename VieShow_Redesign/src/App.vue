cdb3cc86957257d6e8c023a06cb67ef6e247789c
andylinsally <andylinsally@gmail.com>
2026-04-01 10:31:59 +0800

    文字

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
-import BaseButton from './components/Common/BaseButton.vue'
+import Header from '@/components/Layout/Parts/Header.vue'
+import ProcessStep from '@/components/Common/ProcessStep.vue'
+
 </script>
 
 <template>
-    <router-view/>
+  <router-view />
+<header>
+  <Header />
+  <ProcessStep />
+</header>
 </template>
 
 <style lang="scss">
 /* 你的全域樣式 */
-
+body {
+  height: 100vh;
+  background: v.$vieshow-gradient-dark;
+  padding: 133px 0 0; /* 預留 Header 的高度 */
+}
 </style>
