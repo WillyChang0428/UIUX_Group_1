dde0c52993c35b680c0823cc86088c8cee79fda1
WillyChang0428 <ancc405000513@gmail.com>
2026-04-01 10:37:10 +0800

    設定app.vue的style背景

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -4,10 +4,7 @@
 
 <template>
   <router-view />
-<header>
-  <Header />
-  <ProcessStep />
-</header>
+  
 </template>
 
 <style lang="scss">
@@ -15,11 +12,6 @@
 body {
   height: 100vh;
   background: v.$vieshow-gradient-dark;
-<<<<<<< HEAD
 }
 
-=======
-  padding: 133px 0 0; /* 預留 Header 的高度 */
-}
->>>>>>> cdb3cc86957257d6e8c023a06cb67ef6e247789c
 </style>
