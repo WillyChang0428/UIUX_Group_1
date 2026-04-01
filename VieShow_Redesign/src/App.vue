4f540eb0aabc04212c5eb3f15662213679a79987
GomaSeal <wwinm19971001@gmail.com>
2026-04-01 15:19:48 +0800

    宣 15:19

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  
+<SignUpStart/>
+<InputBox/>
 </template>
 
 <script setup>
-
+import SignUpStart from './components/Auth/SignUpStart.vue';
+import InputBox from './components/Common/InputBox/InputBox.vue';
 </script>
 
 <style lang="scss" scoped>
