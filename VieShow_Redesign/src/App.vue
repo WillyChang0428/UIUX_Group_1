95774997538f76b961100370d3879c2748d5f0e9
andylinsally <andylinsally@gmail.com>
2026-04-01 09:29:07 +0800

    芷璿20260401_v1

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-
+import BaseButton from './components/Common/BaseButton.vue'
 </script>
 
 <template>
   <div id="app">
     <router-view></router-view>
     </div>
+    <BaseButton>立即訂票</BaseButton>
 </template>
 
 <style>
