ba667e319108e33def63b243a0dc85243e2c1493
Melody2654 <melodyma54316@gmail.com>
2026-04-02 11:15:57 +0800

    建立彈窗外殼四種樣式

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -4,32 +4,7 @@
 
 <script setup>
 
-
-<template>
-  <FlatSelect :isOpen="isOpen" :isSelected="isSelected" :placeholder="selectedValue || '預設提示文字'"
-    @toggle="isOpen = !isOpen" @clear="selectedValue = ''; isSelected = false">
-    <template #options>
-      <li @click="handleSelect('選項 A')">選項 A</li>
-      <li @click="handleSelect('選項 B')">選項 B</li>
-      <SelectCategory label="雙北">
-        <li @click="handleSelect('選項 A')">選項 A</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-      </SelectCategory>
-      <SelectCategory label="桃竹苗">
-        <li @click="handleSelect('選項 A')">選項 A</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-        <li @click="handleSelect('選項 B')">選項 B</li>
-      </SelectCategory>
-    </template>
-  </FlatSelect>
-</template>
-
-
+</script>
 
 <style lang="scss" scoped>
 
