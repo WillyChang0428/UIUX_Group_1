3c35c1cdc812d290b13f6aa2dc1b16cbfa5792b9
Melody2654 <melodyma54316@gmail.com>
2026-04-02 16:13:50 +0800

    建立限定套票模組

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -1,16 +1,8 @@
-<script setup>
-//null
-</script>
-
 <template>
-<!-- null -->
+
 </template>
 
-<style lang="scss">
-body {
-    height: 100vh;
-    background: v.$vieshow-gradient-dark;
-    padding: 133px 0 0;
-    /* 預留 Header 的高度 */
-};
-</style>
+<script setup>
+
+</script>
+
