1754b91d72ef67805ef9a07919ae12da5fb7128c
Melody <melodyma54316@gmail.com>
2026-04-09 01:24:34 +0800

    建立訂單完成模組_待完成

--- VieShow_Redesign/src/App.vue
+++ VieShow_Redesign/src/App.vue
@@ -1,20 +1,11 @@
+<template>
+
+</template>
+
 <script setup>
 
 </script>
 
-<template>
-  
-</template>
-
-
 <style lang="scss">
-@import "@/assets/scss/variables"; // 💡 確保引入地基
 
-html, body , #app {
-  height: 100vh;
-  /* 使用專案定義的深色漸層 [cite: 1] */
-  background: v.$vieshow-gradient-dark;
-}
 </style>
-
-
